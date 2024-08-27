fea5bb49ead4fc2b87cf12a43f03f60d99a72df4
Anando Gopal Chatterjee <anandogc@gmail.com>
2024-08-27 17:17:57 +0530

    Update send_email_for_pi_approval_and_bank_transfer.py

--- scripts/send_email_for_pi_approval_and_bank_transfer.py
+++ scripts/send_email_for_pi_approval_and_bank_transfer.py
@@ -2,12 +2,6 @@ from HARS.models import Application
 from HARS.models import AccountType
 from HARS.mail import Send_mail
 
-account_type_name = {
-    'PS-RA': 'Param Sanganak - Regular Access',
-    'PS-HPA': 'Param Sanganak - High Priority Access',
-    'HPC2013-QA': 'HPC2013 - Quarterly Access'
-}
-
 def Send_emails_for_account_type(account_type):
 
     account_type = AccountType.objects.get(type_id=account_type)
@@ -30,7 +24,7 @@ def Send_emails_for_account_type(account_type):
             email = f"{a.hpc_profile.institute_profile.user.username}@iitk.ac.in"
             name = a.hpc_profile.institute_profile.name
             id_no = a.hpc_profile.institute_profile.id_no
-            ac_type = account_type_name[account_type]
+            ac_type = account_type.name
             amount = a.amount
 
             #try:
