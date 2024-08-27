2d0c81913a19f89a015237ae5d9849d9de8d7d36
Anando Gopal Chatterjee <anandogc@gmail.com>
2024-08-27 16:35:47 +0530

    Mail updated for bank payment

--- scripts/send_email_for_pi_approval_and_bank_transfer.py
+++ scripts/send_email_for_pi_approval_and_bank_transfer.py
@@ -2,6 +2,12 @@ from HARS.models import Application
 from HARS.models import AccountType
 from HARS.mail import Send_mail
 
+account_type_name = {
+    'PS-RA': 'Param Sanganak - Regular Access',
+    'PS-HPA': 'Param Sanganak - High Priority Access',
+    'HPC2013-QA': 'HPC2013 - Quarterly Access'
+}
+
 def Send_emails_for_account_type(account_type):
 
     account_type = AccountType.objects.get(type_id=account_type)
@@ -24,11 +30,11 @@ def Send_emails_for_account_type(account_type):
             email = f"{a.hpc_profile.institute_profile.user.username}@iitk.ac.in"
             name = a.hpc_profile.institute_profile.name
             id_no = a.hpc_profile.institute_profile.id_no
-
+            ac_type = account_type_name[account_type]
             amount = a.amount
 
             #try:
-            Send_mail("Bank_payment", {'name': name, 'email': email, 'amount': amount, 'id_no': id_no})
+            Send_mail("Bank_payment", {'name': name, 'email': email, 'ac_type': ac_type, 'amount': amount, 'id_no': id_no})
             a.email_sent = True
             a.save()
 
