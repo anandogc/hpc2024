c3f51d32bb3d0d5df11395a397f5526559a364b6
Anando Gopal Chatterjee <anandogc@gmail.com>
2025-02-14 15:34:51 +0530

    Update send_email_for_topup_pi_approval_and_bank_transfer.py

--- scripts/send_email_for_topup_pi_approval_and_bank_transfer.py
+++ scripts/send_email_for_topup_pi_approval_and_bank_transfer.py
@@ -1,41 +1,32 @@
-from HARS.models import Application
+from HARS.models import Topup
 from HARS.models import AccountType
 from HARS.mail import Send_mail
 
-def Send_emails_for_account_type(account_type):
+def Send_emails():
 
-    account_type = AccountType.objects.get(type_id=account_type)
+    # Filter topups in which email has not been sent yet
+    topups = Topup.objects.filter(email_sent=False)
 
-    # Filter applications in which email has not been sent yet
-    applications = Application.objects.filter(account_type=account_type, email_sent=False)
+    for t in topups:
+        if t.user_account.institute_profile.work_profile.pi_profile and t.pi_time is None:  # Group Member
+            email = f"{t.user_account.institute_profile.work_profile.pi_profile.user.username}@iitk.ac.in"
+            name = t.user_account.institute_profile.work_profile.pi_profile.name
 
-    for a in applications:
-        if a.hpc_profile.pi_profile and a.pi_time is None:  # Group Member
-            email = f"{a.hpc_profile.pi_profile.user.username}@iitk.ac.in"
-            name = a.hpc_profile.pi_profile.name
-            #try:
             Send_mail("PI_approval", {'name': name, 'email': email})
-            a.email_sent = True
-            a.save()
-            #except:
-            #    pass
+            t.email_sent = True
+            t.save()
 
-        if a.payment_mode == 'Bank':
-            email = f"{a.hpc_profile.institute_profile.user.username}@iitk.ac.in"
-            name = a.hpc_profile.institute_profile.name
-            id_no = a.hpc_profile.institute_profile.id_no
+        if t.payment_mode == 'Bank':
+            email = f"{t.user_account.institute_profile.user.username}@iitk.ac.in"
+            name = t.user_account.institute_profile.name
+            id_no = t.user_account.institute_profile.id_no
 
-            amount = a.amount
+            amount = t.amount
 
-            #try:
             Send_mail("Bank_payment", {'name': name, 'email': email, 'amount': amount, 'id_no': id_no})
-            a.email_sent = True
-            a.save()
-
-            #except:
-            #    pass
+            t.email_sent = True
+            t.save()
 
 
-Send_emails_for_account_type('PS-RA')
-Send_emails_for_account_type('PS-HPA')
-Send_emails_for_account_type('HPC2013-QA')
+
+Send_emails()
